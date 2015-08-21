726b48f734d9ad29ab54a4808ded60312b5bbbb5
unknown <somratcste@SOMRAT.(none)>
2015-08-21 22:56:34 +0600

    Creat class , object and  function.

---
+++ Accessing_object.php
@@ -0,0 +1,27 @@
+<?php
+class User
+{
+	public $name , $password ;
+	
+	function save_user()
+	{
+		echo ' Save User code goes here';
+	}
+}
+
+$object = new User ;
+print_r($object);
+echo '<br>';
+
+$object->name = "joe";
+$object->password = "mypass";
+print_r($object);
+echo "br";
+
+$object->save_user();
+/**
+*  
+*/
+
+
+?>
